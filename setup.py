48960de23c53385a024c723f7f0d0ff235afc2bf
Simon Conseil <contact@saimon.org>
2020-08-25 22:30:06 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+import numpy as np
+import os
+from setuptools import Extension, find_packages, setup
+from Cython.Build import cythonize
+
+ext_modules = [
+    Extension(
+        "*",
+        ["ndcombine/*.pyx"],
+        include_dirs=[np.get_include()],
+        # libraries=[...],
+        # library_dirs=[...]
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    ),
+]
+
+compiler_directives = {}
+
+if os.getenv('COVERAGE'):
+    print('Adding linetrace directive')
+    compiler_directives['profile'] = True
+    compiler_directives['linetrace'] = True
+    compiler_directives['emit_code_comments'] = True
+    os.environ['CFLAGS'] = ('-g -DCYTHON_TRACE_NOGIL=1 --coverage '
+                            '-fno-inline-functions -O0')
+    gdb_debug = True
+else:
+    gdb_debug = False
+
+ext_modules = cythonize(ext_modules,
+                        compiler_directives=compiler_directives,
+                        gdb_debug=gdb_debug)
+
+setup(
+    name='ndcombine',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    ext_modules=cythonize(ext_modules),
+)
