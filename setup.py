8b282b16c2162e4b2ed8c05f9f713e98cccc8345
Simon Conseil <contact@saimon.org>
2021-10-24 18:34:10 +0200

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -5,12 +5,78 @@
 # other information are set in the setup.cfg file.
 
 import os
+import sys
 
+from setuptools import Extension, setup
 
-import numpy as np
-from Cython.Build import cythonize
-from extension_helpers import add_openmp_flags_if_available
-from setuptools import Extension, find_packages, setup
+# First provide helpful messages if contributors try and run legacy commands
+# for tests or docs.
+
+TEST_HELP = """
+Note: running tests is no longer done using 'python setup.py test'. Instead
+you will need to run:
+
+    tox -e test
+
+If you don't already have tox installed, you can install it with:
+
+    pip install tox
+
+If you only want to run part of the test suite, you can also use pytest
+directly with::
+
+    pip install -e .[test]
+    pytest
+
+For more information, see:
+
+  http://docs.astropy.org/en/latest/development/testguide.html#running-tests
+"""
+
+if 'test' in sys.argv:
+    print(TEST_HELP)
+    sys.exit(1)
+
+DOCS_HELP = """
+Note: building the documentation is no longer done using
+'python setup.py build_docs'. Instead you will need to run:
+
+    tox -e build_docs
+
+If you don't already have tox installed, you can install it with:
+
+    pip install tox
+
+You can also build the documentation with Sphinx directly using::
+
+    pip install -e .[docs]
+    cd docs
+    make html
+
+For more information, see:
+
+  http://docs.astropy.org/en/latest/install.html#builddocs
+"""
+
+if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
+    print(DOCS_HELP)
+    sys.exit(1)
+
+VERSION_TEMPLATE = """
+# Note that we need to fall back to the hard-coded version if either
+# setuptools_scm can't be imported or setuptools_scm can't determine the
+# version, so we catch the generic 'Exception'.
+try:
+    from setuptools_scm import get_version
+    version = get_version(root='..', relative_to=__file__)
+except Exception:
+    version = '{version}'
+""".lstrip()
+
+# Import this later to allow checking deprecated options before
+import numpy as np  # noqa
+from Cython.Build import cythonize  # noqa
+from extension_helpers import add_openmp_flags_if_available  # noqa
 
 extension = Extension(
     "*",
@@ -38,31 +104,6 @@ ext_modules = cythonize([extension],
                         compiler_directives=compiler_directives,
                         gdb_debug=gdb_debug)
 
-VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
-try:
-    from setuptools_scm import get_version
-    version = get_version(root='..', relative_to=__file__)
-except Exception:
-    version = '{version}'
-""".lstrip()
-
-setup(
-    name='ndcombine',
-    description='Fast ND arrays combination',
-    author='Simon Conseil',
-    url='https://github.com/saimn/ndcombine',
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    ext_modules=ext_modules,
-    python_requires='>=3.6',
-    install_requires=['numpy', 'astropy'],
-    extras_require={
-        'tests': ['pytest'],
-    },
-    use_scm_version={'write_to': os.path.join('ndcombine', 'version.py'),
-                     'write_to_template': VERSION_TEMPLATE},
-)
+setup(ext_modules=ext_modules,
+      use_scm_version={'write_to': os.path.join('ndcombine', 'version.py'),
+                       'write_to_template': VERSION_TEMPLATE})
